76f46cdddf7a509295cf74e24822ca71b36abcc7
bloodyfool <wesselb94@gmail.com>
2014-11-21 19:57:19 +0100

    less output more fun

--- time.cpp
+++ time.cpp
@@ -6,7 +6,7 @@ using namespace std;
 Time::Time(int h, int m, int s): hour(h), minute(m), second(s)
 {
     pos = (( m/10 ) + h*6);
-    cout << pos << endl;
+    //cout << pos << endl;
 }
 
 int Time::Hour()
